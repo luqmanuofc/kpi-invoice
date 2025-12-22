9476d48643dc768300761cbdee862fbaa4480c48
luqmanuofc <luqman.bukhari@ucalgary.ca>
2025-12-22 13:00:34 -0700

    chore: remove all migrations

--- prisma/migrations/20251221001044_add_archived_to_invoice/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Invoice" ADD COLUMN     "archived" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20251222043121_refactor_archived_to_status/migration.sql
+++
@@ -1,5 +0,0 @@
--- Step 1: Add ARCHIVED to InvoiceStatus enum
-ALTER TYPE "InvoiceStatus" ADD VALUE 'ARCHIVED';
-
--- Step 2: Drop the archived column
-ALTER TABLE "Invoice" DROP COLUMN "archived";
